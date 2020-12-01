f6171d347cff2e33d32c4ccdf8e4036e9a170d93
lydongcanh <lydongcanh@gmail.com>
2020-12-01 11:08:01 +0700

    Add firebase, react-admin, react-admin-firebase.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Admin, Resource } from 'react-admin';
+import { PostList, PostShow, PostCreate, PostEdit } from "./posts";
+import { FirebaseAuthProvider, FirebaseDataProvider } from 'react-admin-firebase';
 
-function App() {
+const firebaseConfig = {
+  apiKey: "AIzaSyDFqVCSiBM6SJThy-Yqy1i5uXSU7FK2glc",
+  authDomain: "xdpm-thtri.firebaseapp.com",
+  databaseURL: "https://xdpm-thtri.firebaseio.com",
+  projectId: "xdpm-thtri",
+  storageBucket: "xdpm-thtri.appspot.com",
+  messagingSenderId: "194790065519",
+  appId: "1:194790065519:web:5eb0dbcc2a5aed5e2cd49a",
+  measurementId: "G-NQJ794LHLT"
+};
+const firebaseOptions = {};
+const dataProvider = FirebaseDataProvider(firebaseConfig, firebaseOptions);
+const authProvider = FirebaseAuthProvider(firebaseConfig);
+
+export default function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Admin 
+      dataProvider={dataProvider}         
+      authProvider={authProvider}
+    >
+      <Resource 
+        name="posts" 
+        list={PostList} 
+        show={PostShow} 
+        create={PostCreate} 
+        edit={PostEdit} 
+      />
+    </Admin>
   );
 }
-
-export default App;
